858d24ec40f5aa557c4851c2c546bdab139ddec4
Sylvain LE ROUX <sylvain.leroux@telecom-paristech.org>
2016-11-17 08:08:36 +0100

    Correction of the code/ main function so that it compiles

--- code/src/main.c
+++ code/src/main.c
@@ -13,33 +13,15 @@ int main(int argc, char* argv[]) {
     id = 0;
     init(SENSOR_1);
     printf("Start\n");
-    /*for (int i = 0; i < BUFFER_SIZE; i++) {
-        ret = scanf("%i\n", &data);
-        add_value(SENSOR_1, data);
-        id++;
-    }
+    for (int i = 0; i < BUFFER_SIZE; i++)
+        add_value(SENSOR_1, i);
     update_default_value(SENSOR_1);
     while(ret == 1) {
         id++;
         ret = scanf("%i\n", &data);
         add_value(SENSOR_1, data);
-        if (detect_action(SENSOR_1))
-            printf("Touch detected (index: %i, value: %i)\n", id, average[SENSOR_1]);
-        #if DEBUG
-        else
-            printf("Index: %i, in_touch: %i, Average: %i\n", id, in_touch, average[SENSOR_1]);
-        #endif // DEBUG
-    }
-    printf("End (index: %i)\n", id);*/
-    for (int i = 0; i < BUFFER_SIZE; i++) {
-        add_value(SENSOR_1, i);
-    update_default_value();
-    printf("Default value updated: average %i, min = %i, max = %i\n", default_value, default_value - MARGIN, default_value + MARGIN);
-    while(ret == 1) {
-        id++;
-        ret = scanf("%i\n", &data);
-        if (add_value(data))
-            printf("Touch detected (index: %i, average %i)\n", id, average);
+        if (detect_action(SENSOR_1) == 1)
+            printf("Touch detected (index: %i, average %u)\n", id, *average);
     }
     update_default_value(SENSOR_1);
     linear_regression(SENSOR_1);
