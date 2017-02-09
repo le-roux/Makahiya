793df4371e684ce48543ce594b5353133e7467d0
Tanguy ROUFFIGNAC <trouffignac@enst.fr>
2017-02-09 09:27:23 +0100

    HSV to RGB

--- Demo/demo.py
+++ Demo/demo.py
@@ -3,6 +3,41 @@ import websockets
 import time
 import twitter
 
+def setLed(i, h, s, v):
+	t = int((h / 60)) % 6
+	f = float(h) / 60 - t
+	l = v * (1 - s)
+	m = v * (1 - f * s)
+	n = v * (1 - (1 - f) * s)
+	if t == 0:
+		r = v
+		g = n
+		b = l
+	elif t == 1:
+		r = m
+		g = v
+		b = l
+	elif t == 2:
+		r = l
+		g = v
+		b = n
+	elif t == 3:
+		r = l
+		g = m
+		b = v
+	elif t == 4:
+		r = n
+		g = l
+		b = v
+	elif t == 5:
+		r = v
+		g = l
+		b = m
+	r = round(r * 255)
+	g = round(g * 255)
+	b = round(b * 255)
+	print (str(r) + ' ' + str (g) + ' ' + str(b))
+
 async def demo():
 	api = twitter.Api(consumer_key='5AxCp1lqGjdQ123yDmAm6JBcw',
 			  consumer_secret='1UTqusvXqMC0fJeQYCFLQbVVb1DiUyjE2p4Ru3FBd6Msa7N11D',
@@ -24,5 +59,11 @@ async def demo():
 			elif msg == "touch 1 2":
 				api.PostUpdate("Je suis touché(e) !")
 
-asyncio.get_event_loop().run_until_complete(demo())
+# asyncio.get_event_loop().run_until_complete(demo())
+
+while True:
+	h = int(input())
+	s = float(input())
+	v = float(input())
+	setLed(1, h, s, v)
 
